1420cdf425084cec7d79f48bcc86e2f969021de3
xiaoma <M1884536778@163.com>
2019-04-18 21:40:51 +0800

    style

--- src/views/orderNumber/orderNumber.vue
+++ src/views/orderNumber/orderNumber.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="order-number box-style">
-        <div class="border-style">
+        <div class="border-style num-box">
             <mt-field label="快递单号" placeholder="请输入快递单号" readonly v-model="number">
                 <button class="grayBtn blueBtn">获取快递单号</button>
             </mt-field>
             <mt-field label="快递费用" placeholder="请输入快递费用" v-model="money"></mt-field>
-            <button class="grayBtn redBtn">确定</button>
-            <button class="grayBtn blueBtn cancal-btn">取消</button>
+            <div class="btm-btn">
+                <button class="grayBtn redBtn">确定</button>
+                <button class="grayBtn blueBtn cancal-btn">取消</button>
+            </div>
+            
         </div>
     </div>
 </template>
@@ -20,3 +23,15 @@ export default {
     }
 }
 </script>
+<style lang='less'>
+    .num-box{
+        padding-bottom: 10px
+    }
+    .btm-btn{
+        display: flex;
+        justify-content:center;
+        .cancal-btn{
+            margin-left: 80px
+        }
+    }
+</style>
