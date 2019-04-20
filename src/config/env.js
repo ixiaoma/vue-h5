5858e7b14532ab9a9cbb7e6b64414937e2a36a21
jiangyx <2055108207@qq.com>
2019-04-20 21:18:05 +0800

    接口

--- src/config/env.js
+++ src/config/env.js
@@ -5,12 +5,14 @@ if (process.env.NODE_ENV == 'development') {
   baseUrl = 'http://10.100.50.27:26302/'
   // baseUrl='http://192.168.31.81:8080/'
 } else if (process.env.NODE_ENV == 'production') {
-  baseUrl = 'http://10.100.50.27:26302/'
-  // baseUrl='http://192.168.31.81:8080/'
+  // baseUrl = 'http://10.100.50.27:26302/'
+  // baseUrl='http://192.168.31.81:8080/'//增光小米
+  baseUrl='http://10.116.36.153:8080'//增光Lenovo
   env = 'pro';
 } else if (process.env.NODE_ENV == 'beta') {
-  baseUrl = 'http://10.100.50.27:26302/'
+  // baseUrl = 'http://10.100.50.27:26302/'
   // baseUrl='http://192.168.31.81:8080/'
+  baseUrl='http://10.116.36.153:8080'
   env = 'beta';
 }
 
