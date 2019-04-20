42d359edfd6512be56a36ad551296f89ba70e3c7
jiangyx <2055108207@qq.com>
2019-04-20 14:34:33 +0800

    换接口加/

--- src/config/env.js
+++ src/config/env.js
@@ -3,14 +3,14 @@ let routerMode = 'hash';
 let env = 'dev';
 if (process.env.NODE_ENV == 'development') {
   // baseUrl = 'http://10.100.50.27:26302/'
-  baseUrl='http://192.168.31.81:8080'
+  baseUrl='http://192.168.31.81:8080/'
 } else if (process.env.NODE_ENV == 'production') {
   // baseUrl = 'http://10.100.50.27:26302/'
-  baseUrl='http://192.168.31.81:8080'
+  baseUrl='http://192.168.31.81:8080/'
   env = 'pro';
 } else if (process.env.NODE_ENV == 'beta') {
   // baseUrl = 'http://10.100.50.27:26302/'
-  baseUrl='http://192.168.31.81:8080'
+  baseUrl='http://192.168.31.81:8080/'
   env = 'beta';
 }
 
